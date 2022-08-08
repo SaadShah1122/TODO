a33d26e03c20ac5d5c9471f10af5c04c01397f90
Saad1122 <humzi0340@gmail.com>
2022-08-08 19:30:17 +0300

    commit on 'master'

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+//creating Database
+
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function (err, db) {});
